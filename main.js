f481bbc3419272b32c2d965d4f5bb6aa5bb7b3b7
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-15 16:15:49 -0600

    Renderizar listas (Array, Array de objetos (coleccion), Objetos)

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-// Directivas incluidas //
-// -- Renderizado condicional -- //
-
-/*
-      v-show                  -> Muestra u oculta (cambiando la propiedad display a none) elementos html, segun el modelo || El elemento sigue estando en el DOM
-
-
-      Paralelismo con las estructuras de control (if, if else, else) de JS
-            v-if="condicion"        -> Muestra o no (Elimina el elemento del DOM <!---->) elementos segun la condicion
-            v-else-if="condicion"   -> Muestra o no (Elimina el elemento del DOM <!---->) el elemento segun la condicion
-            v-else                  -> Muestra o no (Elimina el elemento del DOM <!---->) Si no se han cumplido las condiciones anteriores
-
-      Se pueden renderizar condicionalmente varios elementos poniendolos dentro de un template
-            <template>
-                  <></>
-                  <></>
-                  <></>
-            </template>
-
-*/
-
 const vm = new Vue ({
      el: 'main',
      data: {
-           conectado: false,
-           edad: 44
-     } 
+           laborales: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'],
+           tareas: [
+                 { nombre: 'Hacer la compra', prioridad: 'baja' },
+                 { nombre: 'Aprender Vue y Firebase', prioridad: 'alta' },
+                 { nombre: 'Ir al gimnasio', prioridad: 'media' }
+           ],
+           persona: {
+                 nombre: 'Juan',
+                 profesion: 'dev',
+                 ciudad: 'Valencia'
+           }
+     }
+
 });
