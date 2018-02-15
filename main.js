59c9f8ff61defd9e7a85fa0f1a18668d9fe8d136
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-14 22:06:19 -0600

    Instalacion (script) y data binding

--- main.js
+++ main.js
@@ -2,7 +2,18 @@
 
 // Vanilla js
 
-const h1 = document.querySelector('h1'),
-      input = document.querySelector('input');
+// const h1 = document.querySelector('h1'),
+//       input = document.querySelector('input');
 
-input.addEventListener('keyup', () => h1.innerText = input.value);
+// input.addEventListener('keyup', () => h1.innerText = input.value);
+
+
+
+// Vue.js
+
+new Vue ({
+      el: 'main',
+      data: {
+            mensaje: 'Hola mundo!!'
+      }
+});
