7e2af480aae94e14886060f09205e0dcfb4faab0
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-15 17:00:17 -0600

    Eventos

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-const vm = new Vue ({
-     el: 'main',
-     data: {
-           laborales: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'],
-           tareas: [
-                 { nombre: 'Hacer la compra', prioridad: 'baja' },
-                 { nombre: 'Aprender Vue y Firebase', prioridad: 'alta' },
-                 { nombre: 'Ir al gimnasio', prioridad: 'media' }
-           ],
-           persona: {
-                 nombre: 'Juan',
-                 profesion: 'dev',
-                 ciudad: 'Valencia'
-           }
-     }
+// Instaladas las Vue Developer Tools en Firefox
 
+/*
+Eventos
+      directiva: v-on:event="method""
+
+      method debe estar definido en el objeto methods de la instancia de Vue
+*/
+
+const vm = new Vue ({
+      el: 'main',
+      data: {
+            nuevaTarea: null,
+            tareas: [
+                  'Aprender Vue.js',
+                  'Aprender ES6',
+                  'Publicar algo todos los dias'
+            ]
+      },
+      methods: {
+            agregarTarea() {
+                  // this, hace referencia a la instancia Vue
+                  this.tareas.unshift(this.nuevaTarea);
+                  this.nuevaTarea = '';
+            }
+      }
 });
+
+
+// Vanilla JS
+// function agregarTarea () {
+//       const input = document.querySelector('input[type=text]');
+//       vm.tareas.unshift(input.value);
+//       input.value = '';
+// }
