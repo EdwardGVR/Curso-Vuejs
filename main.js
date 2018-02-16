c959714cb6cfa35d64203261cd912acf84c2c536
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-16 15:31:54 -0600

    Filtros

--- main.js
+++ main.js
@@ -1,64 +1,48 @@
-// Instaladas las Vue Developer Tools en Firefox
-
 /*
-Propiedades computadas
-      Se declaran dentro del objeto computed de la instancia Vue
-      Se usan para no saturar de logica la vista, ya que esta se realiza de este lado y se retorna el resultado
-
-      computed vs methods
-            Se puede lograr los mismos resultados con ambos, sin embargo, se diferencian en que los
-            methods son re evaluados cada vez que se llaman, en cambio las computed solo se re evaluan
-            cuando cambia el modelo, es decir que si volvemos a llamar una computed propertie sin que el modelo
-            haya cambiado, la segunda llamada sera cargada desde cache.
+Filtros
+      En la version 1 de Vue, se incluian diferentes filtros establecidos para colecciones de datos,
+      actualmente se recomienda crearlas uno mismo como computed properties, sin embargo, es posible usar librerias
+      que brinden las funcionalidades anteriores como underscore o Lodash
 */
+
+
+// El filtro debe estar definido antes de la instancia de Vue
+Vue.filter('alReves', (valor) => valor.split('').reverse().join(''));
 
 const vm = new Vue ({
       el: 'main',
       data: {
-            mensaje: 'Hola VueJS',
-            nuevaTarea: null,
-            tareas: [
+            busqueda: '',
+            minimo: 5,
+            juegos: [
                   {
-                        titulo: 'Aprender Vue.js',
-                        prioridad: true,
-                        antiguedad: 23
+                     titulo: 'Battlefield 1',
+                     genero: 'FPS',
+                     puntuacion: 9            
                   },
                   {
-                        titulo: 'Aprender ES6',
-                        prioridad: true,
-                        antiguedad: 123
+                     titulo: 'Civilization VI',
+                     genero: 'Estrategia',
+                     puntuacion: 10
                   },
                   {
-                        titulo: 'Publicar algo',
-                        prioridad: false,
-                        antiguedad: 378
+                     titulo: 'Resident Evil 7',
+                     genero: 'Survival Horror',
+                     puntuacion: 7
+                  },
+                  {
+                     titulo: 'League of Legends',
+                     genero: 'MOBA',
+                     puntuacion: 4
                   }
             ]
       },
-      methods: {
-            agregarTarea () {
-                  // this, hace referencia a la instancia Vue
-                  this.tareas.unshift(this.nuevaTarea);
-                  this.nuevaTarea = '';
-            }
-      },
-      computed: { // Propiedades computadas
-            mensajeAlReves () {
-                  return this.mensaje.split('').reverse().join('');
+      computed: {
+            mejoresJuegos () {
+                  return this.juegos.filter((juego) => juego.puntuacion >= this.minimo);
             },
-            tareasConPrioridad() {
-                  return this.tareas.filter((tarea) => tarea.prioridad);
-            },
-            tareasPorAntiguedad () {
-                  return this.tareas.sort( (a, b) => b.antiguedad - a.antiguedad );
+            buscarJuego () {
+                  return this.juegos.filter((juego) => juego.titulo.includes(this.busqueda));
             }
       }
 });
-
-
-// Vanilla JS
-// function agregarTarea () {
-//       const input = document.querySelector('input[type=text]');
-//       vm.tareas.unshift(input.value);
-//       input.value = '';
-// }
