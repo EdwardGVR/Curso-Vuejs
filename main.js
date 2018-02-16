d6e8939ffa4f12ff9493324502820772c421d06b
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-16 16:38:20 -0600

    Data binding en atributos y clases (v-bind  shorthand-> : )

--- main.js
+++ main.js
@@ -1,65 +1,30 @@
 /*
-Instancia Vue
-      Es el intermediario entre el DOM y la logica de la aplicacion
 
-      Cuando montamos la instacia Vue, automaticamente se crean getters y setters para las propiedades del modelo qu es lo que hace que sean reactivos.
-            Por lo que si despues de montar la instancia, se le agregan propiedades desde fuera, estas nuevas propiedades no seran reactivas, ya que no 
-            tendran sus respectivos getters y setters
+Data binding en atributos y clases
 
-      Se pueden tener varias instancias Vue
-
-
-      *** Ciclo de vida de la instacia Vue ***
-            -     new Vue ()
-            -     beforeCreate
-            -     created
-            -     beforeMount
-            -     mounted
-
-            ---   Cuando cambia el modelo   ---
-
-                        -     beforeUpdate
-                        -     updated
-            
-            -     beforeDestroy
-            -     destroyed
-
+      Asociar el atributo con una parte del modelo, una expresion que se evaluara
 
 */
 
-
-// Instancia 1
-const vm1 = new Vue ({
+new Vue ({
       el: 'main',
       data: {
-            mensaje: 'Instancia Vue 1'
-      },
-      beforeUpdate() {
-            console.log('beforeUpdate', this.mensaje);
-            
-      },
-      updated() {
-            console.log('updated', this.mensaje);
-            
+            tareas: [
+                  {titulo: 'Hacer la compra', completado: false},
+                  {titulo: 'Aprender Vue.js', completado: false},
+                  {titulo: 'Aprender Firebase', completado: false},
+                  {titulo: 'Dominar ES6', completado: false},
+                  {titulo: 'Salir a correr', completado: false}
+            ]
       },
       methods: {
-            alReves () {
-                  this.mensaje = this.mensaje.split('').reverse().join('');
-                  // vm2.mensaje = 'Cambiado desde instancia 1';
+            completarTarea(tarea) {
+                  tarea.completado = !tarea.completado;   
             }
       },
       computed: {
-            mensajeMayusculas () {
-                  return this.mensaje.toUpperCase();
+            tareasCompletadas () {
+                  return this.tareas.filter((tarea) => tarea.completado);
             }
-      }
-});
-
-
-// Instancia 2
-const vm2 = new Vue ({
-      el: '#app',
-      data: {
-            mensaje: 'Instancia Vue 2'
       }
 });
