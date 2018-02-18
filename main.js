1c3485ceac4ecf3e92a3643948a7c1f920e74241
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-17 21:47:12 -0600

    Ajax con vue-resource

--- main.js
+++ main.js
@@ -1,36 +1,28 @@
 /*
 
-Transiciones
+Utilizar ajax en Vue con vue-resource
 
-      -     Se coloca dentro del componente <transition>...</transition> el elemento al que se le aplicara la transicion
-      -     Se le coloca el atributo name="nombreTransicion" al componente <transition name="nombreTransicion">
-      -     Para que se anime al cargar la pagina (en initial render) se le agrega el atributo appear al componente <transition>
-                  <transition name="nombreTransicion" appear>
-      -     Se crean las clases css correspondientes
-            -     nombreTransicion-enter
-            -     nombreTransicion-enter-active
-            -     nombreTransicion-leave-to
-            -     nombreTransicion-leave-active
-
-      -     En las transiciones entre elementos, si son elementos del mismo tipo (dos <h1>...), es necesario establecer un atributo
-            key="identificador" a cada uno, donde indentificador los diferencia
-            
-            Ademas para evitar que un elemento se superponga o monte al otro durante la transicion, se agrega el atributo
-            modo="" con los posibles valores de in-out, out-in en el componente transition <transition modo="">
-
-      -     Es posible agregar librerias externas de animaciones como animate.css, e indicar sus respectivas clases
 
 */
 
 new Vue ({
       el: 'main',
+      // Cuando la instancia esta lista
+      mounted() {
+            // console.log('Instancia montada');
+            this.cargarPersonas();
+      },
       data: {
-            mostrar: true,
-            mensajes: {
-                  transicion: 'Transiciones con Vue.js',
-                  animacion: 'Animaciones con Vue.js',
-                  animationCustom: 'Animaciones custom cn Vue.js',
-                  entreElementos: 'Transiciones entre elementos con Vue.js'
+            personas: []
+      },
+      methods: {
+            cargarPersonas () {
+                  // Basado en promesas
+                  this.$http.get('https://randomuser.me/api/?results=500')
+                        .then((respuesta) => {
+                              // console.log(respuesta);
+                              this.personas = respuesta.body.results;
+                        });
             }
       }
 });
