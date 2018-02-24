67e779fda60bb1e4138d278d852b58c3d82bd330
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-24 16:47:09 -0600

    Introduccion a componentes

--- main.js
+++ main.js
@@ -2,22 +2,49 @@
 
 Componentes - Introduccion
 
+Los componentes deben declarar su modelo como una funcion para que sean unicos
+
 */
 
-new Vue = ({
-      el: 'main',
+// Vue.prototype.$http = axios;
+
+Vue.component('mi-componente', {
+      template: `<h1>Componente Vue</h1>`
+});
+
+Vue.component('mis-tareas', {
+      // props -> Para comunicarse desde fuera con el componente
+      // props: ['tareas'],
+      template: `<ul><li v-for="tarea in tareas" v-text="tarea.title"></li></ul>`,
       mounted() {
-            this.cargarTodoList();
+            axios.get('https://jsonplaceholder.typicode.com/todos')
+            // .then -> cuando tenga la respuesta
+            .then((respuesta) => {
+                  this.tareas = respuesta.data;
+            });
       },
-      methods: {
-            cargarTodoList () {
-                  axios.get('https://jsonplaceholder.typicode.com/todos')
-                  .then((respuesta) => {
-                        this.tareasAjax = respuesta.body;
-                  });
+      data() {
+            return {
+                  tareas: [],
             }
-      },
-      data: {
-            tareasAjax: []
       }
+});
+
+new Vue ({
+      el: 'main',
+      // mounted() {
+      //       axios.get('https://jsonplaceholder.typicode.com/todos')
+      //       // .then -> cuando tenga la respuesta
+      //       .then((respuesta) => {
+      //             this.tareasAjax = respuesta.data;
+      //       });
+      // },
+      // data: {
+      //       tareasAjax: [],
+      //       tareasLocales: [
+      //             {title: 'Hacer la compra'},
+      //             {title: 'Aprender Vue'},
+      //             {title: 'Aprender ES6'}
+      //       ]
+      // }
 });
